10bc41f82f81a01f325ec1cf0368789268a4a19b
jdhaines3 <32001826+jdhaines3@users.noreply.github.com>
2021-11-24 17:36:24 -0500

    img processing

--- Driver.py
+++ Driver.py
@@ -13,8 +13,8 @@ import keras.layers as lyr
 #--LOAD DATA--#
 
 #import the csvs to a panda dataframe
-trainSet = pd.read_csv('\written_name_train_v2.csv')
-validSet = pd.read_csv('\written_name_validation_v2.csv')
+trainSet = pd.read_csv('\\written_name_train_v2.csv')
+validSet = pd.read_csv('\\written_name_validation_v2.csv')
 #note: back slashes are Windows paths, if using linux or mac, will need to use forward slash
 
 
@@ -61,14 +61,54 @@ validSet = validSet.reset_index(drop=True)
 #trainRows = trainSet.shape[0]
 #validRows = validSet.shape[0]
 ##loading 330k images into memory is not gunna work. Potentially need to do multiple iterations of multiple batches?
-##maybe a subset of images? randomly generated indexes? 
+
+##lets start with subset. For now, say train with 10k images maybe? Placeholder value.
+##can use the xset.shape for statistics though -- total rows after cleaning    
+
 
 #now we need to loop through the CSV's to get the images and store in a list
 #during this, we need to read in the image at the dataframe filename,
-#shape into a 64 * 256 image, either paring down or padding
+#shape into a 64h  256w image
 #then from what I've read, the image needs to be rotated, and pixel intensity set to [0, 1] rather than [0,255]
 
+trainRange = 5000
+validRange = 1000
 
+trainImgLst = []
+trainLblLst = []
+validImgLst = []
+validLblLst = []
+##this will take first trainRange images. If we want to do random, we need to alter a bit
+##One post I read talked about 'blurring' the image to reduce noise... but I'm not sure
+##the code would look like: finalImg = cv2GaussianBlur(greyImg, (5, 5),0)
+    
+for x in range(trainRange):
+    imgPath = '\\train\train\\' + trainSet.loc[x, 'FILENAME']
+    img = cv2.imread(imgPath)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(img, (256, 64))
+    img = img/255
+    img = img.T
+    trainImgLst.append(img)
+    label = str(trainSet.loc[x, 'IDENTITY'])
+    trainLblLst.append(label)
+
+for y in range(validRange):
+    imgPath = '\\train\train\\' + validSet.loc[x, 'FILENAME']
+    img = cv2.imread(imgPath)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(img, (256, 64))
+    img = img/255
+    img = img.T
+    validImgLst.append(img)
+    label = str(trainSet.loc[x, 'IDENTITY'])
+    validLblLst.append(label)
+
+trainImgLst = np.array(trainImgLst).reshape(-1, 256, 64, 1)
+validImgLst = np.array(validImgLst).reshape(-1, 256, 64, 1)    
+
+##convert labels to list of indexes of alphabet
+    
 #--CTC Loss--#
 
 #--Model--#
